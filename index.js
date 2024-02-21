7ad51499c66efd23debaee73d2fde14d147a00f8
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-02-21 17:01:12 +0530

    ok2

--- index.js
+++ index.js
@@ -3,50 +3,27 @@ const axios = require("axios")
 require('dotenv').config();
 
 const app = new App({
-	token: process.env.SLACK_BOT_TOKEN,
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
-	socketMode: true,
-	appToken: process.env.SLACK_APP_TOKEN,
+	token: process.env.SLACK_BOT_TOKEN,
 });
 
-app.command('/hello', async ({ command, ack, say }) => {
-	await ack();
-
-	await say(`Hello, <@${command.user_id}>`);
-});
-
-
-
-app.command('/shorturl', async ({ command, ack, say }) => {
-    await ack();
-
-    // Extract the URL from the command text
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const urls = command.text.match(urlRegex);
-    if (!urls || urls.length === 0) {
-        await say('No URL provided');
-        return;
-    }
-    const longUrl = urls[0];
-
-    try {
-        // Call your URL shortening service
-        const response = await axios.post('https://urlshorten-rzcr.onrender.com/shorten', { originalUrl: longUrl });
-
-        // Extract the shortened URL from the response
-        const shortUrl = response.data.shortUrl;
-
-        await say(`Shortened URL: https://urlshorten-rzcr.onrender.com/${shortUrl}`);
-    } catch (error) {
-        console.error('Error shortening URL:', error);
-        await say('Failed to shorten URL');
-    }
-});
-
-
+app.message('quote',async ({ message, say }) => {
+    
+  
+      try {
+          let resp=await axios.get(`https://api.quotable.io/random`)
+          console.log(message)
+          const quote=resp.data.content;
+          await say(`hello, <@${message.user}>, ${quote}`)
+      } catch (error) {
+        console.error(error);
+        await say('An error occurred while shortening the URL.');
+      }
+   
+  });
 
 (async () => {
-	// Start your app
-	await app.start(process.env.PORT || 8000);
-	console.log(`⚡️ Bot app is running on port ${process.env.PORT || 8000}!`);
-})();
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running!');
+  })();
+
